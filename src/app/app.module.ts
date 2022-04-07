309b11c7b8f094d5dc7abc248caa69e1c0f44136
xArthurDev <arthurrguimaraes12@gmail.com>
2022-04-07 20:11:15 -0300

    feat: add and import libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListFinancialStatementsComponent } from './pages/list-financial-statements/list-financial-statements.component';
+import { RouterModule } from '@angular/router';
+import { FinancialStatementDetailsComponent } from './pages/financial-statement-details/financial-statement-details.component';
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FakeDataService } from './services/fake-data.service';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ListFinancialStatementsComponent, FinancialStatementDetailsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {
+        path: 'list-financial-statements',
+        component: ListFinancialStatementsComponent,
+      },
+      {
+        path: 'financial-statement-details/:id',
+        component: FinancialStatementDetailsComponent,
+      },
+    ]),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    NgbModule,
+    ToastrModule.forRoot(),
+    NgxMaskModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FakeDataService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
