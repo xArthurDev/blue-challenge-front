4081e82b341968757f250f7a338771b21cf301a0
xArthurDev <arthurrguimaraes12@gmail.com>
2022-04-07 20:15:59 -0300

    feat: create financial statement model

---
+++ src/app/models/financial-statement.model.ts
@@ -0,0 +1,16 @@
+export interface FinancialStatement {
+  id: number;
+  name: string;
+  cardNumber: number;
+  type: string;
+  status: string;
+  cardLock: boolean;
+  cardEmission: string;
+  gender: string;
+  birthDate: string;
+  changeChipPassword: string;
+  cardChipPassword: string;
+  cardPasswordValidation: string;
+  travelNoticeException: string;
+  date: string;
+}
