dcaaf98d0064ff3dda24fbbeddda2d5489aea799
xArthurDev <arthurrguimaraes12@gmail.com>
2022-04-07 20:10:23 -0300

    feat: set app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { FinancialStatementDetailsComponent } from './pages/financial-statement-details/financial-statement-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list-financial-statements',
+    pathMatch: 'full',
+  },
+  {
+    path: 'financial-statement-details',
+    component: FinancialStatementDetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
